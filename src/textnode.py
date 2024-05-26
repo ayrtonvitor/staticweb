07e75abb14cd6e0e89375c099f43fd47b6447e39
Ayrton Vitor <ayrtonvitor@pm.me>
2024-05-25 22:57:03 -0300

    Add text types

--- src/textnode.py
+++ src/textnode.py
@@ -11,3 +11,10 @@ class TextNode:
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_image = "image"
+text_type_link = "link"
