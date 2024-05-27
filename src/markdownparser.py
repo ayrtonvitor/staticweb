116acf65581a2272f289c5f339b5262b17379e54
Ayrton Vitor <ayrtonvitor@pm.me>
2024-05-26 21:08:35 -0300

    Implement markdown parser

---
+++ src/markdownparser.py
@@ -0,0 +1,28 @@
+from textnode import TextNode
+from textnode import text_type_italic
+
+class MarkdownParser:
+    def split_nodes_delimiter(self, old_nodes, delimiter, text_type):
+        new_nodes = []
+        for node in old_nodes:
+            new_nodes.extend(self.get_inner_nodes(node, delimiter, text_type))
+
+        return new_nodes
+
+    def get_inner_nodes(self, node, delimiter, text_type):
+        if text_type == text_type_italic and markdown_bold_delimiter in node.text:
+            raise ValueError("Cannot process italic before processing bold")
+        blocks = node.text.split(delimiter)
+        if len(blocks) % 2 == 0:
+            raise ValueError(
+                f'Tag {delimiter} is not closed')
+
+        for i, block in enumerate(blocks):
+            if i % 2 == 1:
+                yield TextNode(block, text_type)
+            else:
+                yield TextNode(block, node.text_type)
+
+markdown_code_delimiter = '`'
+markdown_bold_delimiter = '**'
+markdown_italic_delimiter = '*'
