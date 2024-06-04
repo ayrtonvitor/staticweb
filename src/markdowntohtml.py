5fd7654ce07285f2116743f6215019a7364c6e7c
Ayrton Vitor <ayrtonvitor@pm.me>
2024-06-04 00:53:48 -0300

    Implement inline parsing for more than paragraphs

--- src/markdowntohtml.py
+++ src/markdowntohtml.py
@@ -13,31 +13,51 @@ from textnode import TextNode, text_type_text
 
 
 class MarkdownToHtml:
-    def md_paragraph_to_p(self, block):
-        inline_md_parser = InlineMarkDownParser(TextNode(block['content'], text_type_text))
+    def parse_inline(self, content):
+        inline_md_parser = InlineMarkDownParser(TextNode(content, text_type_text))
 
-        text_nodes = inline_md_parser.text_to_textnodes()
+        return inline_md_parser.text_to_textnodes()
+
+    def md_paragraph_to_p(self, block):
+        text_nodes = self.parse_inline(block['content'])
         leaf_nodes = [node.text_node_to_html_node() for node in text_nodes]
 
         return ParentNode(tag='p', children=leaf_nodes)
 
     def md_header_to_h(self, block):
-        return LeafNode(tag=f"h{block['level']}", value=block['content'])
+        text_nodes = self.parse_inline(block['content'])
+        leaf_nodes = [node.text_node_to_html_node() for node in text_nodes]
+
+        return ParentNode(tag=f"h{block['level']}", children=leaf_nodes)
 
     def md_code_to_pre_code(self, block):
         code = LeafNode(tag='code', value=block['content'])
         return ParentNode(tag='pre', children = [code])
 
     def md_quote_to_blockquote(self, block):
-        quote = LeafNode(tag='p', value=block['content'])
+        text_nodes = self.parse_inline(block['content'])
+        leaf_nodes = [node.text_node_to_html_node() for node in text_nodes]
+        quote = LeafNode(tag='p', value=leaf_nodes)
         return ParentNode(tag='blockquote', children=[quote])
 
     def md_unordered_list_to_ul(self, block):
-        list_items = [LeafNode(tag='li', value=cont) for cont in block['content'].split('\n')]
+        list_items = []
+        for cont in block['content'].split('\n'):
+            text_nodes = self.parse_inline(cont)
+            leaf_nodes = [node.text_node_to_html_node() for node in text_nodes]
+
+            list_items.append(ParentNode(tag='li', children=leaf_nodes))
+
         return ParentNode(tag='ul', children=list_items)
 
     def md_ordered_list_to_ol(self, block):
-        list_items = [LeafNode(tag='li', value=cont) for cont in block['items']]
+        list_items = []
+        for cont in block['items']:
+            text_nodes = self.parse_inline(cont)
+            leaf_nodes = [node.text_node_to_html_node() for node in text_nodes]
+
+            list_items.append(ParentNode(tag='li', children=leaf_nodes))
+
         return ParentNode(tag='ol', children=list_items)
 
     def markdown_to_html_node(self, md_block):
