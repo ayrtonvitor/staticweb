febda2e3ebfad2123d24c3016f6456f7819e9165
Ayrton Vitor <ayrtonvitor@pm.me>
2024-06-04 00:31:53 -0300

    Implement final integration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import os
+from markdowntohtml import MarkdownToHtml
 from pathlib import Path
+import re
 import shutil
-import sys
 
 def copy_dir(from_dir):
     """This is a stupid function writen just for fun. Don't use it for real"""
@@ -36,5 +37,45 @@ def copy_dir(from_dir):
 
     copy(from_dir, public_dir)
 
+def extract_title(markdown):
+    title = re.search(r'(?<!#)# ([^\n]+)', markdown)
+    if not title:
+        raise ValueError("Must contain a header")
+    return title.group(1)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
+
+    with open(from_path, 'r') as file:
+        raw_markdown = file.read()
+
+    md_parser = MarkdownToHtml()
+
+    converted = md_parser.markdown_to_html(raw_markdown)
+    title = extract_title(raw_markdown)
+
+    with open(template_path, 'r') as file:
+        template = file.read()
+
+    template = template.replace('{{ Title }}', title).replace('{{ Content }}', converted)
+
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+
+    with open(os.path.join(dest_path, 'index.html'), 'w') as f:
+        f.write(template)
+
+
+def main():
+    project_root = Path(os.path.abspath(__file__)).parent.parent.absolute()
+    static_path = os.path.join(project_root, 'static')
+    copy_dir(static_path)
+
+    generate_page(
+        os.path.join(project_root, 'content', 'index.md'),
+        os.path.join(project_root, 'template.html'),
+        os.path.join(project_root, 'public'),
+    )
+
 if __name__ == '__main__':
-    copy_dir(sys.argv[1])
+    main()
